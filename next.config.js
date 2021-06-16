ecc7e0a60d69df343254a0edcdecab0f08f72022
Jake Hopking <jake@hopking.io>
2021-06-16 10:36:14 +0100

    Add fix for fs

--- next.config.js
+++ next.config.js
@@ -5,9 +5,19 @@ module.exports = {
     includePaths: [path.join(__dirname, "styles")],
   },
   target: "serverless",
-  resolve: {
-    fallback: {
-      fs: false,
-    },
+  // resolve: {
+  //   fallback: {
+  //     fs: false,
+  //   },
+  // },
+  webpack: (config, {isServer}) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+
+    return config;
   },
 };
