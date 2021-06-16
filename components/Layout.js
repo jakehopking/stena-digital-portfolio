840225232bf79f9536f1d98ce197a53a7700c816
Jake Hopking <jake@hopking.io>
2021-06-16 16:07:33 +0100

    Add border

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styled from "styled-components";
 import Header from "./Header";
-import {SIZE} from "../theme/theme";
+import {SIZE, PALLET} from "../theme/theme";
 import styles from "./Layout.module.scss";
 
 const LayoutContainer = styled.div`
@@ -21,6 +21,8 @@ const SideContainer = styled.div`
   flex-flow: column;
   background-color: var(--color-panel-bg);
   padding: ${SIZE.spacing._5};
+  border-left: 1px solid ${PALLET.grey._00};
+  border-top: 1px solid ${PALLET.grey._00};
 `;
 
 const Layout = ({children, home, sideCol}) => {
