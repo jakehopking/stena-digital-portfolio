c390d4b8a32ba7fe61e36cdd68eece61d5c098b0
Jake Hopking <jake@hopking.io>
2021-06-16 16:07:01 +0100

    Add config and reset

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,20 @@ module.exports = {
     },
   },
   target: node,
+  rules: [
+    {
+      test: /\.scss$/,
+      use: [
+        {
+          loader: "style-loader", // creates style nodes from JS strings
+        },
+        {
+          loader: "css-loader", // translates CSS into CommonJS
+        },
+        {
+          loader: "sass-loader", // compiles Sass to CSS
+        },
+      ],
+    },
+  ],
 };
