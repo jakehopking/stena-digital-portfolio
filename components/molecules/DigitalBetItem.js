b7fd34b89d3a97ce6c6e68a4ed756cf77d5888c1
Jake Hopking <jake@hopking.io>
2021-06-17 11:17:28 +0100

    Add digitalbetitem component

--- components/molecules/DigitalBetItem.js
+++ components/molecules/DigitalBetItem.js
@@ -1,8 +1,21 @@
+import {useEffect, useState} from "react";
+import Circle from "../atoms/Circle";
 import ProgressBar from "../atoms/ProgressBar";
 
 const bigBetText = "Big Bet";
 
 const DigitalBetItem = ({category, state, group, label, progress, bigBet, size}) => {
+  const [colors, setColors] = useState({});
+
+  useEffect(() => {
+    setColors({
+      Now: "--color-primary-0",
+      Next: "--color-secondary-0",
+      Future: "--color-tertiary-0",
+    });
+    return () => {};
+  }, []);
+
   return (
     <div
       className={`o-media o-media--quarter digital-bet-item ${
@@ -10,14 +23,14 @@ const DigitalBetItem = ({category, state, group, label, progress, bigBet, size})
       }`}
     >
       <div className="o-media__fixed digital-bet-item__icons">
-        <div className="">{group}</div>
-        <div className="">{bigBet ? bigBetText : ""}</div>
+        <Circle color={colors[group]} />
+        <div className="big-bet">{bigBet ? bigBetText : ""}</div>
       </div>
       <div className="o-media__fluid digital-bet-item__content">
         <div className="digital-bet-item__main">
-          <div className="">{category}</div>
-          <div className="">{label}</div>
-          <ProgressBar value={progress} />
+          <div className="digital-bet-item__category">{category}</div>
+          <div className="digital-bet-item__label">{label}</div>
+          <ProgressBar value={progress} className="digital-bet-item__progress" />
         </div>
         <div className="digital-bet-item__aside">{state}</div>
       </div>
