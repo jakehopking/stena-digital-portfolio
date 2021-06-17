890049f7e28b9e8018b67ad8b3675d94b87cf404
Jake Hopking <jake@hopking.io>
2021-06-17 11:23:45 +0100

    Import new digitalbetlist component

--- pages/product-teams.js
+++ pages/product-teams.js
@@ -5,35 +5,12 @@ import {AppContext} from "../context/context";
 import Layout, {siteTitle} from "../components/Layout";
 import SidePanel from "../components/sidePanel/SidePanel";
 import OverviewPanel from "../components/sidePanel/OverviewPanel";
-import DigitalBetItem from "../components/molecules/DigitalBetItem";
+import DigitalBetList from "../components/organisms/DigitalBetList";
 import data from "../data/data";
 
 export const databaseId = process.env.NOTION_DB_PORTFOLIO_TRACKER;
 
 const {overviewPanelData, digitalBets} = data;
-
-const DigitalBetList = ({data, title}) => {
-  return (
-    <div className="digital-bet-list">
-      <div className="digital-bet-list__title">{title}</div>
-      <div className="digital-bet-list__list">
-        {data.map((bet, idx) => {
-          return (
-            <DigitalBetItem
-              key={idx + bet.group}
-              category={bet.category}
-              state={bet.state}
-              group={bet.group}
-              label={bet.label}
-              progress={bet.progress}
-              bigBet={bet.bigBet}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
-};
 
 const DashboardStage = ({rows, grid}) => {
   return (
@@ -48,8 +25,12 @@ const DashboardStage = ({rows, grid}) => {
           <div className="o-grid__col u-1/3">
             <DigitalBetList data={digitalBets} title="Now" />
           </div>
-          <div className="o-grid__col u-1/3"></div>
-          <div className="o-grid__col u-1/3"></div>
+          <div className="o-grid__col u-1/3">
+            <DigitalBetList data={digitalBets} title="Next" />
+          </div>
+          <div className="o-grid__col u-1/3">
+            <DigitalBetList data={digitalBets} title="Future" />
+          </div>
         </div>
       </section>
     </div>
