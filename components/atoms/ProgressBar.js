89b21ddb570d3522c7570f9dab9a5617cde54c8f
Jake Hopking <jake@hopking.io>
2021-06-17 11:16:39 +0100

    Add defaults and support for bar and track colour

--- components/atoms/ProgressBar.js
+++ components/atoms/ProgressBar.js
@@ -1,12 +1,15 @@
-const ProgressBar = ({total, value, size, trackColor, color}) => {
+const ProgressBar = ({total, value, size, trackColor, barColor}) => {
   const progressCalc = (value / total) * 100;
 
   return (
-    <div className={`progress-bar ${size ? "progress-bar--" + size : ""}`}>
+    <div
+      className={`progress-bar ${size ? "progress-bar--" + size : ""}`}
+      style={{backgroundColor: `var(${trackColor})`}}
+    >
       <div
         className="progress-bar__bar"
         title={`Progress is ${progressCalc}%`}
-        style={{width: `${progressCalc}%`}}
+        style={{width: `${progressCalc}%`, backgroundColor: `var(${barColor})`}}
       ></div>
     </div>
   );
@@ -16,6 +19,8 @@ ProgressBar.defaultProps = {
   total: 100,
   value: 50,
   size: "small",
+  trackColor: "--color-default-bg",
+  barColor: "--color-default-fg",
 };
 
 export default ProgressBar;
