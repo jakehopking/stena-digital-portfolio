23812da5127e498625272fde5fdbb636cab18e45
Jake Hopking <jake@hopking.io>
2021-06-29 15:46:32 +0100

    Add simple try catch fn for handling broken data pipes

--- utils/general.js
+++ utils/general.js
@@ -1,2 +1,10 @@
 export const greatestValue = (array) =>
   array.reduce((acc, cur) => (cur > acc ? cur : acc), -Infinity);
+
+export const tryFn = (fn, fallback = null) => {
+  try {
+    return fn();
+  } catch (error) {
+    return fallback;
+  }
+};
