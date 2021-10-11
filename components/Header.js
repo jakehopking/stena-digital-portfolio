43c07b4fe8f39d5f71a296a632a4d9bd924094fb
Jake Hopking <jake@hopking.io>
2021-10-11 11:58:17 +0100

    Start new dashbaord

--- components/Header.js
+++ components/Header.js
@@ -20,28 +20,10 @@ const Header = () => {
           <a>Product Teams</a>
         </div>
         <div
-          onClick={() => router.push("/roadmap")}
-          className={`nav-main__item ${isActive("/roadmap")}`}
-        >
-          Roadmap
-        </div>
-        <div
-          onClick={() => router.push("/digital-culture")}
-          className={`nav-main__item ${isActive("/digital-culture")}`}
-        >
-          Digital Culture
-        </div>
-        <div
-          onClick={() => router.push("trends")}
+          onClick={() => router.push("/trends")}
           className={`nav-main__item ${isActive("/trends")}`}
         >
           Trends
-        </div>
-        <div
-          onClick={() => router.push("/capabilities")}
-          className={`nav-main__item ${isActive("/capabilities")}`}
-        >
-          Capabilities
         </div>
       </nav>
     </header>
