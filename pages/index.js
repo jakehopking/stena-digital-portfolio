09141a2e0475452ef6e86ad958294bb0bbc30af4
Jake Hopking <jake@hopking.io>
2021-10-13 16:20:15 +0100

    Fix index

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,32 @@
-import Head from "next/head";
-import Link from "next/link";
-import {getDatabase} from "../lib/notion";
-import {AppContext} from "../context/context";
 import Layout, {siteTitle} from "../components/Layout";
 import SidePanel from "../components/sidePanel/SidePanel";
 import OverviewPanel from "../components/sidePanel/OverviewPanel";
-import {getColumnMultiSelectTags, getColumnSelectTags} from "../utils/notion";
+import DashboardStage from "../components/organisms/DashboardStage";
 import data from "../data/data";
 
 export const databaseId = process.env.NOTION_DB_PORTFOLIO_TRACKER;
 
-const {overviewPanelData} = data;
+const {overviewPanelData, circlePackingOne, circlePackingTwo, cmsData} = data;
 
-const DashboardStage = ({rows, grid}) => {
-  return (
-    <div
-      className={`dashboard-stage ${
-        rows ? "dashboard-stage--rows" : grid ? "dashboard-stage--grid" : ""
-      }`}
-    >
-      <section className="dashboard-stage__chart"></section>
-      <section className="dashboard-stage__info"></section>
-    </div>
-  );
-};
-
-export default function Home({bigBetTags, exploreExploitTags, investmentTags}) {
-  // debugger;
+export default function ProductTeams({cmsData}) {
+  const {projects} = cmsData;
 
   return (
     <Layout
       home
       sideCol={
         <SidePanel>
-          <OverviewPanel
-            {...overviewPanelData({exploreExploitTags, bigBetTags, investmentTags})}
-          />
+          <OverviewPanel {...overviewPanelData({projects})} />
         </SidePanel>
       }
-    >
-      <DashboardStage grid />
-    </Layout>
+    ></Layout>
   );
 }
 
 export const getStaticProps = async () => {
-  const database = await getDatabase(databaseId);
   return {
     props: {
-      notion: database,
-      exploreExploitTags: getColumnSelectTags(database, "Explore / Exploit"),
-      investmentTags: getColumnSelectTags(database, "Investment"),
-      bigBetTags: getColumnMultiSelectTags(database, "Big bet"),
+      cmsData,
     },
     revalidate: 1,
   };
