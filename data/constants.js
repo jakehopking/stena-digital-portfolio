268549a7f97befcf7825622cf82824c564737d7e
Jake Hopking <jake@hopking.io>
2021-11-02 20:14:26 +0900

    try cleaning pem key

--- data/constants.js
+++ data/constants.js
@@ -19,7 +19,7 @@ export const phaseTitlesExploit = phaseTitles.slice(9);
 
 export const googleSheetsAuth = {
   client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
 export const googleSheets = {
