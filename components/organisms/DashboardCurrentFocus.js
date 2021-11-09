45e4b6a06c138a5ad8c49bafa7c3e2a85853c2f9
Jake Hopking <jake@hopking.io>
2021-11-09 15:13:35 +0900

    update for cms data

--- components/organisms/DashboardCurrentFocus.js
+++ components/organisms/DashboardCurrentFocus.js
@@ -1,13 +1,10 @@
-import DigitalBetList from "./DigitalBetList";
 import DashboardResponsiveCirclePacking from "../molecules/DashboardResponsiveCirclePacking";
-import {filterFn, generateChartData} from "../../utils/general";
+import {generateChartData} from "../../utils/general";
 
 import {CHART_SCHEMES, CHART_THEME} from "../../theme/theme";
 
 const DashboardCurrentFocus = ({title, chartData}) => {
   const currentFocusChartData = generateChartData({chartData, label: title});
-  console.log(currentFocusChartData);
-  // debugger;
   return (
     <div className="dashboard dashboard--focus">
       <div className="container u-p-md u-mr-md">
@@ -16,8 +13,8 @@ const DashboardCurrentFocus = ({title, chartData}) => {
           <div className="grid-item">
             <DashboardResponsiveCirclePacking
               data={currentFocusChartData}
-              height={700}
-              colorScheme={CHART_SCHEMES.two}
+              height={800}
+              colorScheme={CHART_SCHEMES.four}
               theme={CHART_THEME}
             />
           </div>
