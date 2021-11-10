0b638b124e99ddf4dafec58dc225a635f69f8eba
hasanavi <hasanavi@gmail.com>
2021-11-10 16:24:36 +0000

    NextJS Auth0 integration

--- components/Header.js
+++ components/Header.js
@@ -1,105 +1,116 @@
-import {useEffect, useContext, useState} from "react";
-import {useRouter} from "next/router";
-import {AppContext} from "../context/appContext";
-import {NetlifyCMSContext} from "../context/netlifyCmsContext";
-import {mainNavPageRoutes} from "../data/constants";
+import { useEffect, useContext, useState } from "react";
+import { useRouter } from "next/router";
+import { AppContext } from "../context/appContext";
+import { NetlifyCMSContext } from "../context/netlifyCmsContext";
+import { mainNavPageRoutes } from "../data/constants";
 import NavLink from "./atoms/NavLink";
 import NavTools from "./molecules/NavTools";
 import Modal from "./molecules/Modal";
 import DashboardEvents from "./organisms/DashboardEvents";
 
 const Header = () => {
-  const {state, dispatch} = useContext(AppContext);
-  const {shortcutImage, events} = useContext(NetlifyCMSContext);
-  const {isPlaying, currentRoute} = state;
-  const [isMapShowing, setIsMapShowing] = useState(false);
-  const [isScreensaverShowing, setIsScreensaverShowing] = useState(false);
-  const router = useRouter();
+	const { state, dispatch } = useContext(AppContext);
+	const { shortcutImage, events } = useContext(NetlifyCMSContext);
+	const { isPlaying, currentRoute } = state;
+	const [isMapShowing, setIsMapShowing] = useState(false);
+	const [isScreensaverShowing, setIsScreensaverShowing] = useState(false);
+	const router = useRouter();
 
-  const isActive = (path) => (router.pathname === path ? "nav-main__item--active" : "");
+	const isActive = (path) =>
+		router.pathname === path ? "nav-main__item--active" : "";
 
-  const nextPage = () => {
-    dispatch({
-      type: "APP_CAROUSEL_SET_ROUTE",
-      payload: currentRoute === mainNavPageRoutes.length - 1 ? 0 : currentRoute + 1,
-    });
-  };
+	const nextPage = () => {
+		dispatch({
+			type: "APP_CAROUSEL_SET_ROUTE",
+			payload:
+				currentRoute === mainNavPageRoutes.length - 1 ? 0 : currentRoute + 1,
+		});
+	};
 
-  const onToggleCarousel = () => {
-    dispatch({
-      type: isPlaying ? "APP_CAROUSEL_STOP" : "APP_CAROUSEL_START",
-    });
-  };
+	const onToggleCarousel = () => {
+		dispatch({
+			type: isPlaying ? "APP_CAROUSEL_STOP" : "APP_CAROUSEL_START",
+		});
+	};
 
-  const onToggleMap = () => {
-    setIsMapShowing(!isMapShowing);
-    dispatch({
-      type: "APP_CAROUSEL_STOP",
-    });
-  };
+	const onToggleMap = () => {
+		setIsMapShowing(!isMapShowing);
+		dispatch({
+			type: "APP_CAROUSEL_STOP",
+		});
+	};
 
-  const onToggleScreensaver = () => {
-    setIsScreensaverShowing(!isScreensaverShowing);
-    dispatch({
-      type: "APP_CAROUSEL_STOP",
-    });
-  };
+	const onToggleScreensaver = () => {
+		setIsScreensaverShowing(!isScreensaverShowing);
+		dispatch({
+			type: "APP_CAROUSEL_STOP",
+		});
+	};
 
-  useEffect(() => {
-    if (isPlaying) {
-      setTimeout(() => {
-        nextPage();
-      }, 30000);
-      router.push(mainNavPageRoutes[currentRoute].route);
-    }
-    return () => {
-      clearTimeout();
-    };
-  }, [currentRoute, isPlaying]);
+	useEffect(() => {
+		if (isPlaying) {
+			setTimeout(() => {
+				nextPage();
+			}, 30000);
+			router.push(mainNavPageRoutes[currentRoute].route);
+		}
+		return () => {
+			clearTimeout();
+		};
+	}, [currentRoute, isPlaying]);
 
-  return (
-    <header
-      className={`header-main ${
-        (isMapShowing || isScreensaverShowing) && "header-main--withModal"
-      }`}
-    >
-      <img className="header-main__logo" src="/logo_stena.svg" />
-      <nav className="nav-main">
-        {!isMapShowing &&
-          !isScreensaverShowing &&
-          mainNavPageRoutes.map((link, idx) => (
-            <NavLink
-              key={idx + "_" + link.title}
-              route={link.route}
-              title={link.title}
-              isActive={isActive}
-              router={router}
-            />
-          ))}
-        <NavTools
-          isPlaying={isPlaying}
-          isMapShowing={isMapShowing}
-          isScreensaverShowing={isScreensaverShowing}
-          onToggleCarousel={onToggleCarousel}
-          onToggleMap={onToggleMap}
-          onToggleScreensaver={onToggleScreensaver}
-        />
-      </nav>
-      <div className="modal-container">
-        <Modal onClose={onToggleMap} open={isMapShowing} size="withNav" closeType={null}>
-          <img src={shortcutImage.shortcut_image} onClick={onToggleMap} />
-        </Modal>
-        <Modal onClose={onToggleScreensaver} open={isScreensaverShowing} size="withNav">
-          <div className="u-mh-5x u-mv-3x">
-            <DashboardEvents eventData={events.events} title="&nbsp;" />
-          </div>
-          <div className="modal__bg-image">
-            <img src="/images/photos/stena_photo_9.jpg" />
-          </div>
-        </Modal>
-      </div>
-    </header>
-  );
+	return (
+		<header
+			className={`header-main ${
+				(isMapShowing || isScreensaverShowing) && "header-main--withModal"
+			}`}
+		>
+			<img className="header-main__logo" src="/logo_stena.svg" />
+			<nav className="nav-main">
+				{!isMapShowing &&
+					!isScreensaverShowing &&
+					mainNavPageRoutes.map((link, idx) => (
+						<NavLink
+							key={idx + "_" + link.title}
+							route={link.route}
+							title={link.title}
+							isActive={isActive}
+							router={router}
+						/>
+					))}
+				<NavTools
+					isPlaying={isPlaying}
+					isMapShowing={isMapShowing}
+					isScreensaverShowing={isScreensaverShowing}
+					onToggleCarousel={onToggleCarousel}
+					onToggleMap={onToggleMap}
+					onToggleScreensaver={onToggleScreensaver}
+				/>
+			</nav>
+			<div className="modal-container">
+				<Modal
+					onClose={onToggleMap}
+					open={isMapShowing}
+					size="withNav"
+					closeType={null}
+				>
+					<img src={shortcutImage.shortcut_image} onClick={onToggleMap} />
+				</Modal>
+				<Modal
+					onClose={onToggleScreensaver}
+					open={isScreensaverShowing}
+					size="withNav"
+				>
+					<div className="u-mh-5x u-mv-3x">
+						<DashboardEvents eventData={events.events} title="&nbsp;" />
+					</div>
+					<div className="modal__bg-image">
+						<img src="/images/photos/stena_photo_9.jpg" />
+					</div>
+				</Modal>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,81 +1,84 @@
 import "../styles/main.scss";
-import {useState, Fragment, useReducer} from "react";
+import { useState, Fragment, useReducer } from "react";
 import Head from "next/head";
-import {ThemeProvider} from "styled-components";
-import {NetlifyCMSContext} from "../context/netlifyCmsContext";
-import {AppContext} from "../context/appContext";
-import {COLOUR, SIZE, PALLET} from "../theme/theme";
-import {GlobalStyles} from "../styles/global";
-import {cmsData} from "../data/data";
+import { ThemeProvider } from "styled-components";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import { NetlifyCMSContext } from "../context/netlifyCmsContext";
+import { AppContext } from "../context/appContext";
+import { COLOUR, SIZE, PALLET } from "../theme/theme";
+import { GlobalStyles } from "../styles/global";
+import { cmsData } from "../data/data";
 
 const initialAppState = {
-  isPlaying: false,
-  currentRoute: 0,
+	isPlaying: false,
+	currentRoute: 0,
 };
 
 const appStateReducer = (state, action) => {
-  // debugger;
-  switch (action.type) {
-    case "APP_CAROUSEL_START":
-      return {
-        ...state,
-        isPlaying: true,
-      };
-    case "APP_CAROUSEL_STOP":
-      return {
-        ...state,
-        isPlaying: false,
-      };
-    case "APP_CAROUSEL_SET_ROUTE":
-      return {
-        ...state,
-        currentRoute: action.payload,
-      };
-    default:
-      return state;
-  }
+	// debugger;
+	switch (action.type) {
+		case "APP_CAROUSEL_START":
+			return {
+				...state,
+				isPlaying: true,
+			};
+		case "APP_CAROUSEL_STOP":
+			return {
+				...state,
+				isPlaying: false,
+			};
+		case "APP_CAROUSEL_SET_ROUTE":
+			return {
+				...state,
+				currentRoute: action.payload,
+			};
+		default:
+			return state;
+	}
 };
 
-function Application({Component, pageProps}) {
-  const [theme, setTheme] = useState("dark");
-  const [state, dispatch] = useReducer(appStateReducer, initialAppState);
+function Application({ Component, pageProps }) {
+	const [theme, setTheme] = useState("dark");
+	const [state, dispatch] = useReducer(appStateReducer, initialAppState);
 
-  const toggleTheme = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  };
-  // console.log(COLOUR.theme[theme]);
-  // debugger;
-  return (
-    <Fragment>
-      <Head>
-        <title>Digital Portfolio</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <NetlifyCMSContext.Provider value={cmsData}>
-        <AppContext.Provider
-          value={{
-            state,
-            dispatch,
-          }}
-        >
-          <ThemeProvider
-            theme={{
-              color: COLOUR.theme[theme],
-              size: SIZE,
-              pallet: PALLET,
-            }}
-          >
-            {/* <button onClick={toggleTheme}>Switch Theme</button> */}
-            <GlobalStyles />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </AppContext.Provider>
-      </NetlifyCMSContext.Provider>
-    </Fragment>
-  );
+	const toggleTheme = () => {
+		theme === "light" ? setTheme("dark") : setTheme("light");
+	};
+	// console.log(COLOUR.theme[theme]);
+	// debugger;
+	return (
+		<Fragment>
+			<Head>
+				<title>Digital Portfolio</title>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+			<NetlifyCMSContext.Provider value={cmsData}>
+				<AppContext.Provider
+					value={{
+						state,
+						dispatch,
+					}}
+				>
+					<ThemeProvider
+						theme={{
+							color: COLOUR.theme[theme],
+							size: SIZE,
+							pallet: PALLET,
+						}}
+					>
+						{/* <button onClick={toggleTheme}>Switch Theme</button> */}
+						<GlobalStyles />
+						<UserProvider>
+							<Component {...pageProps} />
+						</UserProvider>
+					</ThemeProvider>
+				</AppContext.Provider>
+			</NetlifyCMSContext.Provider>
+		</Fragment>
+	);
 }
 
 export default Application;
